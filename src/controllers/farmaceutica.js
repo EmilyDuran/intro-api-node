50e0c3e24f365b8cbb6bc2d28b3538c65a3493dc
EmilyDuran <emilydiane27@outlook.com>
2025-03-24 20:40:33 -0300

    criação connection

---
+++ src/controllers/farmaceutica.js
@@ -0,0 +1,11 @@
+const db = require('../dataBase/connection');
+
+module.exports = {
+    async listarFarmaceutica(request, response){
+        try{
+            return response.status(200).json({
+                
+            })
+        }
+    }
+}
