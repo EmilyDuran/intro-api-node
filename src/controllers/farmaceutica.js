4acb35ce68497d3a577da9404e161ff2aeb575e0
EmilyDuran <emilydiane27@outlook.com>
2025-03-31 20:38:09 -0300

    aula

--- src/controllers/farmaceutica.js
+++ src/controllers/farmaceutica.js
@@ -4,8 +4,16 @@ module.exports = {
     async listarFarmaceutica(request, response){
         try{
             return response.status(200).json({
-                
-            })
+                sucesso: true,
+                mensagem: 'Lista de Farmaceuticas.',
+                dados: null
+            });
+        }catch(error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.mensage
+            });
         }
     }
 }
