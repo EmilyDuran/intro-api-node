aa50085b8cb934aa12289e5ea7e0d91c23e4a906
EmilyDuran <emilydiane27@outlook.com>
2025-03-31 22:27:22 -0300

    Update farmaceutica.js

--- src/controllers/farmaceutica.js
+++ src/controllers/farmaceutica.js
@@ -5,10 +5,17 @@ module.exports = {
         try{
             return response.status(200).json({
                 sucesso: true,
+<<<<<<< Updated upstream
                 mensagem: 'Lista de Farmaceuticas.',
                 dados: null
             });
         }catch(error) {
+=======
+                mensagem: 'Lista de Produções.',
+                dados: null
+            });
+        }catch (error) {
+>>>>>>> Stashed changes
             return response.status(500).json({
                 sucesso: false,
                 mensagem: 'Erro na requisição.',
